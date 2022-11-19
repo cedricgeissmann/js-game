71b232f0fae7143ca793aa58fc8204fb36206f26
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-11-19 14:19:05 +0100

    Added sprint spell

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let dy = 0
+let dx = 0
+let speed = 0.2
+
+
+class Spell {
+    constructor () {
+        this.name = "Sprint"
+        this.value = 1
+        this.duration = 2000
+        this.cooldown = 5
+    }
+
+    cast() {
+
+    }
+
+    finish() {
+        
+    }
+}
+
 function $(selector) {
     return document.querySelector(selector)
 }
@@ -13,7 +35,36 @@ function newGame() {
 
 function gameLoop() {
     const player = $("#player")
+    let posX = parseFloat(player.getAttribute("x"))
     let posY = parseFloat(player.getAttribute("y"))
-    player.setAttribute("y", posY + 1)
+    player.setAttribute("x", posX + dx * speed)
+    player.setAttribute("y", posY + dy * speed)
     window.requestAnimationFrame(gameLoop)
+}
+
+window.onkeydown = function(ev) {
+    if (ev.code === "ArrowRight") {
+        dx = 1
+    } else if (ev.code === "ArrowLeft") {
+        dx = -1
+    } else if (ev.code === "ArrowUp") {
+        dy = -1
+    } else if (ev.code === "ArrowDown") {
+        dy = 1
+    } else if (ev.code === "Space") {
+        let spell = new Spell()
+        let valBefore = speed
+        speed = speed + spell.value
+        setTimeout(function() {
+            speed = valBefore
+        }, spell.duration)
+    }
+}
+
+window.onkeyup = function(ev) {
+    if (ev.code === "ArrowLeft" || ev.code === "ArrowRight") {
+        dx = 0
+    } else if (ev.code === "ArrowUp" || ev.code === "ArrowDown") {
+        dy = 0
+    }
 }
