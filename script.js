d5c8007f43344d03497480154490e5665c4ce587
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-11-19 15:54:17 +0100

    added player object

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let dy = 0
 let dx = 0
 let speed = 0.2
+let player
 
 const NS = "http://www.w3.org/2000/svg"
 const projectiles = []
@@ -17,8 +18,10 @@ class Vector {
 
     normalize() {
         let len = this.length()
+        if (len > 0) {
         this.x = this.x / len
         this.y = this.y / len
+        }
         return this
     }
 
@@ -29,10 +32,22 @@ class Vector {
     }
 }
 
-class Projectile {
+class GameObject {
     constructor(pos, dir, speed) {
         this.pos = pos
         this.dir = dir.normalize().scale(speed)
+        
+    }
+
+    updatePosition() {
+        this.pos.x = this.pos.x + this.dir.x
+        this.pos.y = this.pos.y + this.dir.y
+    }
+}
+
+class Projectile extends GameObject {
+    constructor(pos, dir, speed) {
+        super(pos, dir, speed)
         this.projectile = document.createElementNS(NS, "circle")
         this.projectile.setAttribute("cx", pos.x)
         this.projectile.setAttribute("cy", pos.y)
@@ -43,13 +58,30 @@ class Projectile {
     }
 
     updatePosition() {
-        this.pos.x = this.pos.x + this.dir.x
-        this.pos.y = this.pos.y + this.dir.y
+        super.updatePosition()
         this.projectile.setAttribute("cx", this.pos.x)
         this.projectile.setAttribute("cy", this.pos.y)
-
     }
 }
+
+class Player extends GameObject {
+    constructor(pos, dir, speed) {
+        super(pos, dir, speed)
+        this.obj = document.createElementNS(NS, "circle")
+        this.obj.setAttribute("cx", pos.x)
+        this.obj.setAttribute("cy", pos.y)
+        this.obj.setAttribute("r", 10)
+        this.obj.setAttribute("fill", "black")
+        document.querySelector("#game > svg").appendChild(this.obj)
+    }
+
+    updatePosition() {
+        super.updatePosition()
+        this.obj.setAttribute("cx", this.pos.x)
+        this.obj.setAttribute("cy", this.pos.y)
+    }
+}
+
 
 class Spell {
     constructor () {
@@ -78,6 +110,8 @@ function newGame() {
     menu.style.display = "none"
     game.style.display = "block"
 
+    player = new Player(new Vector(50, 50), new Vector(0, 0), 1)
+
     window.requestAnimationFrame(gameLoop)
 }
 
@@ -85,25 +119,21 @@ function gameLoop() {
     projectiles.forEach(function(proj) {
         proj.updatePosition()
     })
-    const player = $("#player")
-    let posX = parseFloat(player.getAttribute("x"))
-    let posY = parseFloat(player.getAttribute("y"))
-    player.setAttribute("x", posX + dx * speed)
-    player.setAttribute("y", posY + dy * speed)
+    player.updatePosition()
     window.requestAnimationFrame(gameLoop)
 }
 
 window.onkeydown = function(ev) {
     if (ev.code === "ArrowRight") {
-        dx = 1
+        player.dir.x = 1
     } else if (ev.code === "ArrowLeft") {
-        dx = -1
+        player.dir.x = -1
     } else if (ev.code === "ArrowUp") {
-        dy = -1
+        player.dir.y = -1
     } else if (ev.code === "ArrowDown") {
-        dy = 1
+        player.dir.y = 1
     } else if (ev.code === "Space") {
-        let proj = new Projectile(new Vector(50, 50), new Vector(Math.random()-0.5,Math.random()-0.5), 0.2)
+        let proj = new Projectile(new Vector(player.pos.x, player.pos.y), new Vector(Math.random()-0.5,Math.random()-0.5), 0.2)
         let spell = new Spell()
         let valBefore = speed
         speed = speed + spell.value
@@ -115,8 +145,8 @@ window.onkeydown = function(ev) {
 
 window.onkeyup = function(ev) {
     if (ev.code === "ArrowLeft" || ev.code === "ArrowRight") {
-        dx = 0
+        player.dir.x = 0
     } else if (ev.code === "ArrowUp" || ev.code === "ArrowDown") {
-        dy = 0
+        player.dir.y = 0
     }
 }
